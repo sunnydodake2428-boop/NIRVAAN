374645dc210b4ef8f5d002d24f09446a8da02893
sunnydodake2428-boop <sunnydodake2428@gmail.com>
2026-09-21 10:05:53 +0530

    fix: import requireRole in authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("../middleware/authMiddleware");
+const { verifyToken, requireRole } = require("../middleware/authMiddleware");
 const { signup, login, updateProfile, getProfile, googleLogin } = require("../controllers/authController");
 
 router.post("/signup", signup);
